31a5b3d0460b5429c1805487a09d8037dbd67252
Pedro Alves <pedroalves0409@gmail.com>
2022-07-13 18:52:16 -0300

    the end of session

--- python_programming_for_data_science_and_machine_learning/codes/life.py
+++ python_programming_for_data_science_and_machine_learning/codes/life.py
@@ -1,1 +1,10 @@
+import math
+
 theAnwser = 42
+
+def quote_marvin():
+    print('I\'ve calculated your chance of survival, but I don\'t think you\'ll like it.')
+
+def square_root(x):
+    result = math.sqrt(x)
+    return result
